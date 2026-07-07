ea6f9749dd16c32747767648ace34c773d4b928c
Malcolm Morgan <mem48@users.noreply.github.com>
2026-07-07 12:10:22 +0100

    Approve popualtions scotland
    Documentation and removing old code

--- R/population_scotland.R
+++ R/population_scotland.R
@@ -1,11 +1,13 @@
 # https://www.nrscotland.gov.uk/statistics-and-data/statistics/statistics-by-theme/population/population-estimates/small-area-population-estimates-2011-data-zone-based/time-series
 # Population in 2011 DataZones
-#' Download Scotland Population
+#' Download NRS small-area population estimates for Scotland
 #'
-#' @description Download the scotland population resource and return the local file path.
-#' @details This function is used as part of the pipeline input ingestion stage.
-#' @param path File or directory path.
-#' @return The local path or file name of the downloaded resource.
+#' @description Downloads the NRS SAPE time-series spreadsheets (2001-2021,
+#'   2011 Data Zones) into `path`. Skipped if the folder already contains
+#'   exactly 21 xlsx files. Not currently wired to a target (the
+#'   `population_scot` target assumes files are already present).
+#' @param path Folder to store the downloads; created if missing.
+#' @return `path`.
 #' @keywords internal
 download_scotland_population = function(path = file.path(parameters$path_data,"population_scotland")){
   if(!dir.exists(path)){
@@ -28,12 +30,18 @@ download_scotland_population = function(path = file.path(parameters$path_data,"p
 }
 
 
-#' Load Scotland Population
+#' Load Scottish population estimates 2001-2022 on 2011 Data Zones
 #'
-#' @description Load scotland population data from the source path and return it as an R object.
-#' @details This function is used as part of the pipeline input ingestion stage.
-#' @param path File or directory path.
-#' @return A data frame containing the loaded dataset.
+#' @description Reads the NRS SAPE spreadsheets for 2001-2022 (one per year,
+#'   `popYYYY.xlsx`, "Persons" sheet), converts single year of age to 5-year
+#'   bands and stacks the years. The Data Zone code column is named `LSOA11CD`
+#'   for consistency with the E&W data. Used by the `population_scot` target,
+#'   which is interpolated onto 2022 Data Zones by
+#'   `interpolate_population_dz11_dz22()`. Note: expects `pop2022.xlsx` to
+#'   exist even though `download_scotland_population()` only fetches to 2021.
+#' @param path Folder of `popYYYY.xlsx` files.
+#' @return A data frame with `year`, `LSOA11CD` (2011 DZ code), `all_ages` and
+#'   5-year age-band columns "0-4" ... "90+".
 #' @keywords internal
 load_scotland_population = function(path = file.path(parameters$path_data,"population_scotland")){
 
@@ -65,148 +73,23 @@ load_scotland_population = function(path = file.path(parameters$path_data,"popul
   pops
 }
 
-# Make single population dataset for E,S,W from 2002 to 2021 + E&W 2022
-# Note this uses 2011 DataZones but 2021 LSOAs
-#' Combine Populations
+
+#' Combine the E&W and Scottish population/household series into one GB table
 #'
-#' @description Combine populations inputs into a single consolidated result.
-#' @details This function is used to prepare intermediate analysis tables for later pipeline targets.
-#' @param population_2002_2020 Input object or parameter named `population_2002_2020`.
-#' @param population_2021 Input object or parameter named `population_2021`.
-#' @param population_2022 Input object or parameter named `population_2022`.
-#' @param population_scot Input object or parameter named `population_scot`.
-#' @param lookup_lsoa_2011_21) Lookup table used to map area codes or classifications.
-#' @return A combined data frame or table merging the provided inputs.
-#' @keywords internal
-combine_populations = function(population_2002_2020, population_2021, population_2022, population_scot, lookup_lsoa_2011_21) {
-
-  #TODO: Get Scotland 2022 population
-  population_2002_2020$`85+` = population_2002_2020$`85-89` + population_2002_2020$`90+`
-  population_2002_2020$`85-89` = NULL
-  population_2002_2020$`90+` = NULL
-
-  lookup_lsoa_2011_21 = lookup_lsoa_2011_21[,c("LSOA11CD","LSOA21CD","CHGIND")]
-
-  lookup_lsoa_2011_21_U = lookup_lsoa_2011_21[lookup_lsoa_2011_21$CHGIND == "U",]
-  lookup_lsoa_2011_21_M = lookup_lsoa_2011_21[lookup_lsoa_2011_21$CHGIND == "M",]
-  lookup_lsoa_2011_21_S = lookup_lsoa_2011_21[lookup_lsoa_2011_21$CHGIND == "S",]
-  lookup_lsoa_2011_21_X = lookup_lsoa_2011_21[lookup_lsoa_2011_21$CHGIND == "X",]
-
-  population_2002_2020_U = population_2002_2020[population_2002_2020$LSOA11CD %in% lookup_lsoa_2011_21_U$LSOA11CD,]
-  population_2002_2020_M = population_2002_2020[population_2002_2020$LSOA11CD %in% lookup_lsoa_2011_21_M$LSOA11CD,]
-  population_2002_2020_S = population_2002_2020[population_2002_2020$LSOA11CD %in% lookup_lsoa_2011_21_S$LSOA11CD,]
-  population_2002_2020_X = population_2002_2020[population_2002_2020$LSOA11CD %in% lookup_lsoa_2011_21_X$LSOA11CD,]
-
-  # Merge
-  population_2002_2020_M = dplyr::left_join(population_2002_2020_M, lookup_lsoa_2011_21_M, by = "LSOA11CD")
-  population_2002_2020_M = dplyr::group_by(population_2002_2020_M, year, LSOA21CD)
-  population_2002_2020_M = dplyr::summarise(population_2002_2020_M,
-                                            all_ages = sum(all_ages),
-                                            `0-4` = sum(`0-4`),
-                                            `5-9` = sum(`5-9`),
-                                            `10-14` = sum(`10-14`),
-                                            `15-19` = sum(`15-19`),
-                                            `20-24` = sum(`20-24`),
-                                            `25-29` = sum(`25-29`),
-                                            `30-34` = sum(`30-34`),
-                                            `35-39` = sum(`35-39`),
-                                            `40-44` = sum(`40-44`),
-                                            `45-49` = sum(`45-49`),
-                                            `50-54` = sum(`50-54`),
-                                            `55-59` = sum(`55-59`),
-                                            `60-64` = sum(`60-64`),
-                                            `65-69` = sum(`65-69`),
-                                            `70-74` = sum(`70-74`),
-                                            `75-79` = sum(`75-79`),
-                                            `80-84` = sum(`80-84`),
-                                            `85+` = sum(`85+`))
-  population_2002_2020_M = dplyr::ungroup(population_2002_2020_M)
-
-  # Split
-  split_pop = population_2021[,c("LSOA21","all_ages")]
-  names(split_pop) = c("LSOA21","pop2021")
-  split_pop = dplyr::left_join(lookup_lsoa_2011_21_S, split_pop, by = c("LSOA21CD" = "LSOA21"))
-  split_pop = dplyr::group_by(split_pop, LSOA11CD)
-  split_pop = dplyr::mutate(split_pop, pop_ratio = pop2021 / sum(pop2021))
-  split_pop = dplyr::ungroup(split_pop, LSOA21CD)
-
-  population_2002_2020_S = dplyr::left_join(split_pop, population_2002_2020_S,
-                                            by = "LSOA11CD", relationship = "many-to-many")
-  population_2002_2020_S = as.data.frame(population_2002_2020_S)
-
-  for(i in 7:25){
-    population_2002_2020_S[i] = round(population_2002_2020_S[,i ,drop = TRUE] * population_2002_2020_S$pop_ratio)
-  }
-
-  # Other
-  # b11 = bounds_lsoa11_full[bounds_lsoa11_full$LSOA11CD %in% lookup_lsoa_2011_21_X$LSOA11CD,]
-  # b21 = bounds_lsoa21_full[bounds_lsoa21_full$LSOA21CD %in% lookup_lsoa_2011_21_X$LSOA21CD,]
-  #
-  # tm_shape(b11) +
-  #   tm_fill("blue", alpha = 0.1) +
-  #   tm_borders() +
-  # tm_shape(b21) +
-  #   tm_fill("red", alpha = 0.1) +
-  #   tm_borders()
-  # Other changes are very subtle so go for a 1 to 1 match
-
-  lookup_lsoa_2011_21_X = lookup_lsoa_2011_21_X[!(lookup_lsoa_2011_21_X$LSOA11CD == "E01027506" &
-                                                    lookup_lsoa_2011_21_X$LSOA21CD == "E01035624"),]
-  lookup_lsoa_2011_21_X = lookup_lsoa_2011_21_X[!(lookup_lsoa_2011_21_X$LSOA11CD == "E01008187" &
-                                                    lookup_lsoa_2011_21_X$LSOA21CD == "E01035637"),]
-
-  lookup_lsoa_2011_21_X = lookup_lsoa_2011_21_X[!(lookup_lsoa_2011_21_X$LSOA11CD == "E01023964" &
-                                                    lookup_lsoa_2011_21_X$LSOA21CD == "E01035581"),]
-  lookup_lsoa_2011_21_X = lookup_lsoa_2011_21_X[!(lookup_lsoa_2011_21_X$LSOA11CD == "E01023679" &
-                                                    lookup_lsoa_2011_21_X$LSOA21CD == "E01035608"),]
-
-  lookup_lsoa_2011_21_X = lookup_lsoa_2011_21_X[!(lookup_lsoa_2011_21_X$LSOA11CD == "E01023508" &
-                                                    lookup_lsoa_2011_21_X$LSOA21CD == "E01035582"),]
-  lookup_lsoa_2011_21_X = lookup_lsoa_2011_21_X[!(lookup_lsoa_2011_21_X$LSOA11CD == "E01023768" &
-                                                    lookup_lsoa_2011_21_X$LSOA21CD == "E01035609"),]
-
-  population_2002_2020_X = dplyr::left_join(lookup_lsoa_2011_21_X, population_2002_2020_X, by = "LSOA11CD")
-
-  population_2002_2020_U$LSOA21CD = population_2002_2020_U$LSOA11CD
-
-  nms = c("year","LSOA21CD","all_ages","0-4","5-9","10-14","15-19","20-24",
-          "25-29","30-34","35-39","40-44","45-49",
-          "50-54","55-59","60-64","65-69","70-74","75-79","80-84","85+")
-
-  population_2002_2020_U = population_2002_2020_U[,nms]
-  population_2002_2020_M = population_2002_2020_M[,nms]
-  population_2002_2020_S = population_2002_2020_S[,nms]
-  population_2002_2020_X = population_2002_2020_X[,nms]
-
-  population_2022$year = 2022
-  population_2022$`85+` = population_2022$`85-89` + population_2022$`90+`
-  population_2022 = population_2022[,nms]
-
-  names(population_2021)[names(population_2021) == "LSOA21"] = "LSOA21CD"
-
-  population_2020_2021 = rbind( population_2002_2020_U,
-                                population_2002_2020_M,
-                                population_2002_2020_S,
-                                population_2002_2020_X,
-                                population_2021, population_2022)
-
-  names(population_scot)[names(population_scot) == "LSOA11CD"] = "LSOA21CD"
-  population_scot = population_scot[population_scot$year > 2001,]
-  population_scot$`85+` = population_scot$`85-89` + population_scot$`90+`
-  population_scot  = population_scot[,nms]
-  population_2020_2021 = rbind(population_2020_2021, population_scot)
-  population_2020_2021
-
-}
-
-# New version that uses the VOA/Council Tax data
-#' Combine Populations2
-#'
-#' @description Combine populations2 inputs into a single consolidated result.
-#' @details This function is used to prepare intermediate analysis tables for later pipeline targets.
-#' @param population_households_historical Input object or parameter named `population_households_historical`.
-#' @param population_scot_dz22) Input object or parameter named `population_scot_dz22)`.
-#' @return A combined data frame or table merging the provided inputs.
+#' @description Builds the master `population` target by appending the
+#'   Scottish 2022 Data Zone series (from
+#'   `interpolate_population_dz11_dz22()`) to the E&W series (from
+#'   `extrapolate_population_households()`, which uses VOA/council-tax data).
+#'   Scottish age bands 85-89 and 90+ are collapsed to "85+", column names are
+#'   aligned (`DataZone22` -> `LSOA21CD`, `households` -> `households_est`)
+#'   and an adult count is added for Scotland.
+#' @param population_households_historical E&W population and households per
+#'   2021 LSOA per year.
+#' @param population_scot_dz22 Scottish population and households per 2022
+#'   Data Zone per year.
+#' @return A GB data frame with `year`, `LSOA21CD`, `all_ages`, age bands
+#'   "0-4" ... "85+", `all_properties`, `households_est`, `adults` and
+#'   `adults_per_household` (NA for Scotland).
 #' @keywords internal
 combine_populations2 = function(population_households_historical, population_scot_dz22) {
 
